e41769db3fe7719aa0e29e866a8dda755ada0172
AggrehO <richardsonlyon911@gmail.com>
2021-08-05 09:54:56 +0100

    Completed the Home and  Checkout pages with fulle e-commerce functionality

---
+++ src/components/Header/Header.js
@@ -0,0 +1,51 @@
+import React from "react";
+import logo from "../../assets/amazon_logo.png";
+import "./Header.css";
+import SearchIcon from "@material-ui/icons/Search";
+import ShoppingBasketIcon from "@material-ui/icons/ShoppingBasket";
+import { Link } from "react-router-dom";
+import { useStateValue } from "../Context/StateProvider";
+
+function Header() {
+  const [{ basket }, dispatch] = useStateValue();
+  return (
+    <div className="header">
+      <Link to="/">
+        <img className="header__logo" src={logo} alt="Amazon logo" />
+      </Link>
+
+      <div className="header__search">
+        <input className="header__searchInput" type="text" />
+        <SearchIcon className="header__searchIcon" />
+      </div>
+
+      <div className="header__nav">
+        <div className="header__options">
+          <span className="header__optionLineOne">Hello Guest</span>
+          <span className="header__optionLineTwo">Sign in</span>
+        </div>
+
+        <div className="header__options">
+          <span className="header__optionLineOne">Returns</span>
+          <span className="header__optionLineTwo">& Orders</span>
+        </div>
+
+        <div className="header__options">
+          <span className="header__optionLineOne">Your</span>
+          <span className="header__optionLineTwo">Prime</span>
+        </div>
+
+        <Link to="/checkout">
+          <div className="header__optionBasket">
+            <ShoppingBasketIcon />
+            <span className="header__optionLineTwo header__basketCount">
+              {basket?.length}
+            </span>
+          </div>
+        </Link>
+      </div>
+    </div>
+  );
+}
+
+export default Header;
---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyBm0k9z1DxlgzmJZYNbrB2HKi01SwDqNaU",
+  authDomain: "clone-12628.firebaseapp.com",
+  projectId: "clone-12628",
+  storageBucket: "clone-12628.appspot.com",
+  messagingSenderId: "418254803054",
+  appId: "1:418254803054:web:8067b700215136e7b8167f",
+  measurementId: "G-GS0YJQDJ6F"
+};
